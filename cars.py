6f988ada9e5030dff9681606ff5e8050dd5ae417
Sho . <shouq@example.com>
2024-01-31 10:52:41 +0300

    this is v3 from local 2

--- cars.py
+++ cars.py
@@ -1,4 +1,5 @@
 import random
+from turtle import xcor
 
 print("this is the  version 1")
 
@@ -6,3 +7,8 @@ car= ["KIA" , "HONDA" , "SUZUKI"]
 num = random.randint(0 , len(car))
 
 print(car[num])
+
+year =[1992 , 2233, 4443]
+x = random.randint(0 , len(car))
+
+print(car[x])
